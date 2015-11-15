a686ee74b7dcd2d03a9b60fb22e54f03e4fc0093
Chrysostomus <mattitheman@gmail.com>
2015-11-15 22:23:40 +0700

    Update summondesktop.sh

--- bin/summondesktop.sh
+++ bin/summondesktop.sh
@@ -4,11 +4,11 @@
 #     summonworkspace.sh {1-9,0}
   D=^{$@}; \
   M=$(bspc query --monitors --desktop $D); \
-  if [[ $(bspc query --desktops --monitor $M | wc -l) -gt 1 ]]; then \
-    if [[ $(bspc query --desktops --desktop focused) != $D ]]; then \
+  if [ $(bspc query --desktops --monitor $M | wc -l) -gt 1 ]; then \
+    if [ $(bspc query --desktops --desktop focused) != $D ]; then \
       bspc desktop $D --to-monitor focused; \
       bspc desktop $D --focus; \
     fi; \
-  elif [[ $(bspc query --monitors --monitor focused) != $M ]]; then \
+  elif [ $(bspc query --monitors --monitor focused) != $M ]; then \
     bspc desktop $(bspc query --monitors --desktop $D):focused --swap $(bspc query -M -m focused):focused; \
   fi
