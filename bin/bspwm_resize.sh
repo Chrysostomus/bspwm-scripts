8766f64d25eaf39eb2404c86ab9417b45f3da878
Chrysostomus <mattitheman@gmail.com>
2016-01-29 21:19:12 +0700

    Update bspwm_resize.sh

--- bin/bspwm_resize.sh
+++ bin/bspwm_resize.sh
@@ -1,79 +1,42 @@
 #!/bin/bash
-# Originally by https://github.com/windelicato/, modified to do different actions to windows
-# Sample sxhkd usage:
-#
-# alt + {a,f}
-#    /home/sunn/scripts/bspwm_resize {-p,-n} -x -s 20
-#
-# alt + {s,d}
-#    /home/sunn/scripts/bspwm_resize {-p,-n} -y -s 20
 
-POSITIVE=false
-HORIZONTAL=false
-SIZE='20'
+size=${2:-'20'}
+dir=$1
 
-err() {
-        echo "$1"
-        exit 1
-}
-
-
-usage() {
-        echo "usage: bspwm_resize [direction]"
-        echo
-        echo "Options:"
-        echo "   -p, --positive       - resize in positively"
-        echo "   -n, --negative       - resize in negatively"
-        echo "   -x, --xdir           - resize in x direction"
-        echo "   -y, --ydir           - resize in y dir"
-        echo "   -s,                  - number of pixels to resize or move"
-        echo "   -h, --help           - display this"
-        exit
-}
-
-if [[ $# -eq 0 ]] ; then
-        usage
-        exit
-fi
-
-for i in "$@"; do
-        case $i in
-                '-p'|'--positive')
-                        POSITIVE=true
-                        ;;
-                '-n'|'--negative')
-                        POSITIVE=false
-                        ;;
-                '-x'|'--xdir')
-                        HORIZONTAL=true
-                        ;;
-                '-y'|'--ydir')
-                        HORIZONTAL=false
-                        ;;
-                '-s')
-                        SIZE=$(echo $@ | sed 's/.*-s \([0-9]*\).*/\1/')
-                        [[ "$SIZE" == "$@" ]] && err "Must specify number of pixels"
-                        ;;
-                ''|'-h'|'--help')
-                        usage
-                        exit
-                        ;;
-                *)
-                        ;;
-        esac
-done
-
+bspc config pointer_follows_focus true
 # Find current window mode
-TWINDOW_STATE="$(bspc wm -d -w | grep -o '"state":"tiled"')"
+is_tiled() {
+bspc query -T -n | grep -q '"state":"tiled"'
+}
+# If the window is floating, move it
+if ! is_tiled; then
+#only parse input if window is floating,tiled windows accept input as is
+        case "$dir" in
+  		west) switch="-w"
+  		sign="-"
+        	;;
+  		east) switch="-w"
+         	sign="+"
+       		;;
+  		north) switch="-h"
+         	sign="-"
+        	;;
+  		*) switch="-h"
+     		sign="+"
+     		;;
+ 		esac
+ xdo resize ${switch} ${sign}${size}
 
-# If the window is tiled, change split ration
-if [[ $WINDOW_STATE == '"state":"tiled"' ]] ; then
-        $HORIZONTAL && switch=('west' 'east') || switch=('south' 'north')
-        $POSITIVE && sign="+" || sign="-"
-        bspc node -e ${switch[0]} ${sign}${SIZE} ||  bspc node -e ${switch[1]} ${sign}${SIZE}s
-# Otherwise, window is floating or pseudotiled, resize it
+# Otherwise, window is tiled: switch with window in given direction
 else
-        $HORIZONTAL && switch="-w" || switch="-h"
-        $POSITIVE && sign="+" || sign="-"
-        xdo resize ${switch} ${sign}${SIZE}
+     case "$dir" in
+  		west) bspc node @west -r -$size
+        	;;
+  		east) bspc node @west -r +$size
+       		;;
+  		north) bspc node @south -r +$size
+        	;;
+  		*) bspc node @south -r +$size
+     		;;
+ 		esac
 fi
