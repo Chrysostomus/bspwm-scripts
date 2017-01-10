b5fb5ce6da40599daaa128f9ba21cb9cd6c753f5
Chrysostomus <mattitheman@gmail.com>
2017-01-10 15:22:08 +0200

    Create DidIClickDesktop.sh

---
+++ bin/DidIClickDesktop.sh
@@ -0,0 +1,10 @@
+#!/bin/dash
+# Did I just click the desktop?
+
+#Get the window id of the window that is under the pointer and set variable WINDOW to it
+eval $(xdotool getmouselocation --shell)
+
+#Get the window id of the rootwindow aka desktop and convert the id into decimal form that xdotool uses so we can easily compare the values
+rootwindowid=$(($(lsw | head -1)))
+#Compare window ids to see if window under mouse is the rootwindow. Returns false if not.
+[ "$WINDOW" = "$rootwindowid" ]
